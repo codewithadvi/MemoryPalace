46197555def46ca6d5a0de4d0fdb2cc0c5dff706
codewithadvi <advithiya.duddu@example.com>
2026-02-23 01:39:59 +0530

    chore: remove start.bat from tracking

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start cmd /k "cd backend && npm run dev"
-start cmd /k "cd frontend && npm run dev"
-echo Servers are starting...
-echo Once started:
-echo - Frontend will run on http://localhost:5173
-echo - Backend will run on http://localhost:3000
